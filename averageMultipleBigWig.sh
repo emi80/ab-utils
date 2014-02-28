6984100ebc331e583a53c36be6e9a00cc7203f4c
Alessandra Breschi <alessandra.breschi@crg.es>
2014-02-28 15:00:14 +0100

    generate bw and delete bedgraph

--- averageMultipleBigWig.sh
+++ averageMultipleBigWig.sh
@@ -2,9 +2,10 @@
 
 out=$1
 count=0
+chromsizes=$2
 
-if [ $# != 1 ]; then
-	echo "USAGE: cat file.list.txt | $0 out_prefix"
+if [ $# != 2 ]; then
+	echo "USAGE: cat file.list.txt | $0 out_prefix chrom.sizes"
 	exit 1
 fi
 
@@ -15,6 +16,8 @@ done
 
 
 /usr/bin/time -o ${out}.time bigWigMerge $fileStr stdout 2>/dev/null | awk -v T=$count '{$4=$4/T; print}' > ${out}.bedGraph
+/software/rg/el6.3/bin/bedGraphToBigWig ${out}.bedGraph $chromsizes ${out}.bw
+rm ${out}.bedGraph
 
 exit 0
 
